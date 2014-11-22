c66d8cd833b6d74c109367ba4f19ecfaa00dcb3b
Christhian <christhianjesus@gmail.com>
2014-11-21 22:12:28 -0430

    Mas errores

--- DynamicError.rb
+++ DynamicError.rb
@@ -9,6 +9,13 @@ class DivisionCero < DynamicError
   end
 end
 
+class ErrorLectura < DynamicError
+  def initialize(token)
+    print ("Error entre la línea #{token.line} y columna #{token.column}: Error de lectura.")
+    exit -1
+  end
+end
+
 class OperacionNoDefinida < DynamicError
   def initialize(token)
     print ("Error entre la línea #{token.line} y columna #{token.column}: Se intento evaluar una matriz con un solo parametro.")
